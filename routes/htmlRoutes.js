30a17ef96212adfc5e95f19a426ebeed0d98ab81
Mano <Koutouzosm@gmail.com>
2022-09-15 15:14:53 -0400

    saving

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const router = require('express').Router();
+
+
+// Create our routes here
+
+// create route for notes.html
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+// Use * to send any other route hit back to home page
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+module.exports = router;
